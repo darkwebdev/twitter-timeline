4571dd1f52096e9c88dea60035bd7e51fc2cbe32
tibalt <gtibalt@gmail.com>
2015-11-22 17:30:23 +0100

    added date output

--- src/Tweet.js
+++ src/Tweet.js
@@ -4,6 +4,7 @@ module.exports = React.createClass({
     render: function() {
         return (
             <article className="tweet">
+                <time>{ this.props.data.created_at }</time>
                 <p>{ this.props.data.text }</p>
             </article>
         );
